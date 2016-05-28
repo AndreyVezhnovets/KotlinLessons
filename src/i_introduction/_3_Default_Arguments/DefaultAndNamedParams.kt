5d056990952a831c75a6e220ffc4d2484009438b
Andreyv1989 <andrey.v1989@mail.ru>
2016-05-28 10:26:19 +0300

    дефолтные параметры в методе

--- src/i_introduction/_3_Default_Arguments/DefaultAndNamedParams.kt
+++ src/i_introduction/_3_Default_Arguments/DefaultAndNamedParams.kt
@@ -4,22 +4,22 @@ import util.TODO
 import util.doc2
 
 fun todoTask3(): Nothing = TODO(
-    """
+        """
         Task 3.
         Several overloads of 'JavaCode3.foo()' can be replaced with one function in Kotlin.
         Change the declaration of the function 'foo' in a way that makes the code using 'foo' compile.
         You have to add parameters and replace 'todoTask3()' with a real body.
         Uncomment the commented code and make it compile.
     """,
-    documentation = doc2(),
-    references = { name: String -> JavaCode3().foo(name); foo(name) })
+        documentation = doc2(),
+        references = { name: String -> JavaCode3().foo(name); foo(name) })
 
-fun foo(name: String): String = todoTask3()
+fun foo(name: String, number: Int = 42, toUpperCase: Boolean = false) = (if (toUpperCase) name.toUpperCase() else name) + number
 
 fun task3(): String {
-    todoTask3()
-//    return (foo("a") +
-//            foo("b", number = 1) +
-//            foo("c", toUpperCase = true) +
-//            foo(name = "d", number = 2, toUpperCase = true))
+    //  todoTask3()
+    return (foo("a") +
+            foo("b", number = 1) +
+            foo("c", toUpperCase = true) +
+            foo(name = "d", number = 2, toUpperCase = true))
 }
