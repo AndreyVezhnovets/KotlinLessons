a660fd9c828a4d49e4e3751c52a9e5864ef8a711
Andreyv1989 <andrey.v1989@mail.ru>
2016-05-28 12:05:24 +0300

    конструкция when is , вместо switch case

--- src/i_introduction/_8_Smart_Casts/SmartCasts.kt
+++ src/i_introduction/_8_Smart_Casts/SmartCasts.kt
@@ -6,11 +6,12 @@ import util.doc8
 interface Expr
 class Num(val value: Int) : Expr
 class Sum(val left: Expr, val right: Expr) : Expr
-
+class Rop(val left: Expr, val right: Expr, val odin: Expr) : Expr
 fun eval(e: Expr): Int =
         when (e) {
-            is Num -> todoTask8(e)
-            is Sum -> todoTask8(e)
+            is Num -> e.value
+            is Sum -> eval(e.left) + eval(e.right)
+            is Rop -> eval(e.odin)- eval(e.odin)
             else -> throw IllegalArgumentException("Unknown expression")
         }
 
