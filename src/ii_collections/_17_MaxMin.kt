36510049efce1fa4176ccd70ec3d7f794db9e252
Andreyv1989 <andrey.v1989@mail.ru>
2016-05-29 12:49:12 +0300

    поиск наименьшего и наибольшего значения в коллекции

--- src/ii_collections/_17_MaxMin.kt
+++ src/ii_collections/_17_MaxMin.kt
@@ -1,16 +1,19 @@
 package ii_collections
 
+import ii_collections.data.customers
+
 fun example4() {
     val max = listOf(1, 42, 4).max()
     val longestString = listOf("a", "b").maxBy { it.length }
 }
 
 fun Shop.getCustomerWithMaximumNumberOfOrders(): Customer? {
-    // Return a customer whose order count is the highest among all customers
-    todoCollectionTask()
+    return customers.maxBy { it.orders.size }
+    // todoCollectionTask()
 }
 
 fun Customer.getMostExpensiveOrderedProduct(): Product? {
     // Return the most expensive product which has been ordered
-    todoCollectionTask()
+   // todoCollectionTask()
+    return orders.flatMap { it.products}.toSet().maxBy { it.price }
 }
