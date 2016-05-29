1d675414c48eb5857552b43f40436227e23b3ca3
Andreyv1989 <andrey.v1989@mail.ru>
2016-05-29 17:40:36 +0300

    заимплементил Comparable для MyDate переопределил compareTo

--- src/iii_conventions/MyDate.kt
+++ src/iii_conventions/MyDate.kt
@@ -1,6 +1,23 @@
 package iii_conventions
 
-data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int)
+data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int) : Comparable<MyDate> {
+    override fun compareTo(date2: MyDate): Int = when {
+
+        month > date2.month -> 1
+        month < date2.month -> -1
+        else -> {
+            when {
+                   dayOfMonth> date2.dayOfMonth -> 1
+                dayOfMonth< date2.dayOfMonth -> -1
+                else -> {
+                    0
+                }
+            }
+        }
+    }
+
+}
+
 
 operator fun MyDate.rangeTo(other: MyDate): DateRange = todoTask27()
 
