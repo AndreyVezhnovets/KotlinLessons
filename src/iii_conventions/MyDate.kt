44bea8a1128352176242b0b842d48621d25d548c
Andreyv1989 <andrey.v1989@mail.ru>
2016-05-29 18:15:42 +0300

    реализовал rangeTo в MyDate

--- src/iii_conventions/MyDate.kt
+++ src/iii_conventions/MyDate.kt
@@ -20,7 +20,7 @@ data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int) : Comparab
     }
 }
 
-operator fun MyDate.rangeTo(other: MyDate): DateRange = todoTask27()
+operator fun MyDate.rangeTo(other: MyDate): DateRange = DateRange(this,other)
 
 enum class TimeInterval {
     DAY,
